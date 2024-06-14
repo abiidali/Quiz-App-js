0873cc29ec3b7abc519c7a0f4d71627c3facf0c3
Abid Ali <143890772+abiidali@users.noreply.github.com>
2024-06-14 12:53:45 -0700

    Add files via upload

---
+++ Js/question.js
@@ -0,0 +1,58 @@
+
+var questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "b). Hyper Text Markup Language",
+    options: [
+      "a). Hyper Text Preprocessor",
+      "b). Hyper Text Markup Language",
+      "c). Hyper Text Multiple Language",
+      "d). Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "d). Cascading Style Sheet",
+    options: [
+      "a). Common Style Sheet",
+      "b). Colorful Style Sheet",
+      "c). Computer Style Sheet",
+      "d). Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the best teacher in SMIT?",
+    answer: "c). Sir Jaffar Aman",
+    options: [
+      "a). Sir Bilal",
+      "b). Sir Raja Ehsan",
+      "c). Sir Jaffar Aman",
+      "d). None of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does DOM stand for?",
+    answer: "c). Document Object Model",
+    options: [
+      "a). Document Oscar Model",
+      "b). Document Object Master",
+      "c). Document Object Model",
+      "d). Domain Object Model"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you call a function named, myFunction?",
+    answer: "b). myFunction()",
+    options: [
+      "a). call myFunction()",
+      "b). myFunction()",
+      "c). myFunction() call",
+      "d). myfunction()"
+    ]
+  },
+];
